c7a3603c51a51c87823f4d995c21ce57c51ca515
rupendra-yadav <yadavruppu15@gmail.com>
2025-10-25 19:19:27 +0530

    api changes

--- server/config.py
+++ server/config.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+import os
 
+# Load variables from .env file
 load_dotenv()
 
 class Settings(BaseSettings):
-    TAVILY_API_KEY:str = "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"
+    TAVILY_API_KEY: str = os.getenv("TAVILY_API_KEY")
+
